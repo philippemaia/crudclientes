4577bec3466c3526651f511b2ac89ea0e80d9bc3
Philippe Maia <philippe.m.maia@hotmail.com>
2025-01-29 00:14:13 -0300

    Atualização de cliente atualiza cliente com dados válidos

--- src/main/java/com/desafiophilippe/crudclientes/services/ClientService.java
+++ src/main/java/com/desafiophilippe/crudclientes/services/ClientService.java
@@ -38,6 +38,14 @@ public class ClientService {
         return new ClientDTO(entity);
     }
 
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client entity = repository.getReferenceById(id);
+        copyDtotoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
     private void copyDtotoEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
